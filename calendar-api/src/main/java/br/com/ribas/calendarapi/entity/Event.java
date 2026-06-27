759ae8965417d2c532ccd5410109ddc414eb4a17
Joao Ribas <jv.simao10@gmail.com>
2026-06-27 02:05:15 -0300

    feat: classe event e dependências do back

---
+++ calendar-api/src/main/java/br/com/ribas/calendarapi/entity/Event.java
@@ -0,0 +1,30 @@
+package br.com.ribas.calendarapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "events")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private LocalDateTime startDateTime;
+
+    @Column(nullable = false)
+    private LocalDateTime endDateTime;
+
+    private String description;
+
+    private String color;
+
+}
