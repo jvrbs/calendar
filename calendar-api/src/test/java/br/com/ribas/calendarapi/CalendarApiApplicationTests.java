37503b618748e039e00d781d2b7a4cc31134de33
Joao Ribas <jv.simao10@gmail.com>
2026-06-27 01:39:02 -0300

    feat: arquitetura do projeto

---
+++ calendar-api/src/test/java/br/com/ribas/calendarapi/CalendarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ribas.calendarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
